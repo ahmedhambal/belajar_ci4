82d8e921b1a025ed7cc060b9a6d22fe1b10d939c
AHMAD HAMBALI <hambaliahmad88@gmail.com>
2021-05-18 09:50:31 +0700

    Update Tcpdf

---
+++ app/Controllers/Tcpdf_example.php
@@ -0,0 +1,136 @@
+<?php namespace App\Controllers;
+
+use TCPDF;
+
+class Tcpdf_example extends BaseController
+{
+	public function e001()
+	{
+		// create new PDF document
+		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+		// set document information
+		$pdf->SetCreator(PDF_CREATOR);
+		$pdf->SetAuthor('Nicola Asuni');
+		$pdf->SetTitle('TCPDF Example 001');
+		$pdf->SetSubject('TCPDF Tutorial');
+		$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+		// set default header data
+		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE . ' 001', PDF_HEADER_STRING, [0, 64, 255], [0, 64, 128]);
+		$pdf->setFooterData([0, 64, 0], [0, 64, 128]);
+
+		// set header and footer fonts
+		$pdf->setHeaderFont([PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN]);
+		$pdf->setFooterFont([PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA]);
+
+		// set default monospaced font
+		$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+		// set margins
+		$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+		$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+		$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+		// set auto page breaks
+		$pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+
+		// set image scale factor
+		$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+		// ---------------------------------------------------------
+
+		// set default font subsetting mode
+		$pdf->setFontSubsetting(true);
+
+		// Set font
+		// dejavusans is a UTF-8 Unicode font, if you only need to
+		// print standard ASCII chars, you can use core fonts like
+		// helvetica or times to reduce file size.
+		$pdf->SetFont('dejavusans', '', 14, '', true);
+
+		// Add a page
+		// This method has several options, check the source code documentation for more information.
+		$pdf->AddPage();
+
+		// set text shadow effect
+		$pdf->setTextShadow(['enabled' => true, 'depth_w' => 0.2, 'depth_h' => 0.2, 'color' => [196, 196, 196], 'opacity' => 1, 'blend_mode' => 'Normal']);
+
+		// Set some content to print
+		$html_code = view('Tcpdf/e001');
+		$html      = <<<EOD
+		{$html_code}
+		EOD;
+
+		// Print text using writeHTMLCell()
+		$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+		// ---------------------------------------------------------
+		$this->response->setContentType('application/pdf');
+		// Close and output PDF document
+		// This method has several options, check the source code documentation for more information.
+		$pdf->Output('example_001.pdf', 'I');
+
+		//============================================================+
+		// END OF FILE
+		//============================================================+
+	}
+
+	public function e002()
+	{
+		// create new PDF document
+		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+		// set document information
+		$pdf->SetCreator(PDF_CREATOR);
+		$pdf->SetAuthor('Nicola Asuni');
+		$pdf->SetTitle('TCPDF Example 002');
+		$pdf->SetSubject('TCPDF Tutorial');
+		$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+		// remove default header/footer
+		$pdf->setPrintHeader(false);
+		$pdf->setPrintFooter(false);
+
+		// set default monospaced font
+		$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+		// set margins
+		$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+
+		// set auto page breaks
+		$pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+
+		// set image scale factor
+		$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+		// ---------------------------------------------------------
+
+		// set font
+		$pdf->SetFont('times', 'BI', 20);
+
+		// add a page
+		$pdf->AddPage();
+
+		// set some text to print
+		$txt = <<<EOD
+		TCPDF Example 002
+
+		Default page header and footer are disabled using setPrintHeader() and setPrintFooter() methods.
+		EOD;
+
+		// print a block of text using Write()
+		$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+
+		// ---------------------------------------------------------
+
+		//Close and output PDF document
+		$this->response->setContentType('application/pdf');
+		$pdf->Output('example_002.pdf', 'I');
+
+		//============================================================+
+		// END OF FILE
+		//============================================================+
+	}
+
+}
