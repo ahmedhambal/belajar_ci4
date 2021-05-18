d8f413dd12615d35fab74b5e25eb5f11c279dc91
ahmedhambal <hambaliahmad88@gmail.com>
2021-05-18 10:24:07 +0700

    Update Tcpdf
    Example 003

--- app/Controllers/Tcpdf_example.php
+++ app/Controllers/Tcpdf_example.php
@@ -133,4 +133,72 @@ EOD;
 		//============================================================+
 	}
 
+	public function e003()
+	{
+		// create new PDF document
+		$pdf = new \App\Libraries\Mypdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+		// set document information
+		$pdf->SetCreator(PDF_CREATOR);
+		$pdf->SetAuthor('Nicola Asuni');
+		$pdf->SetTitle('TCPDF Example 003');
+		$pdf->SetSubject('TCPDF Tutorial');
+		$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+		// set default header data
+		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
+
+		// set header and footer fonts
+		$pdf->setHeaderFont([PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN]);
+		$pdf->setFooterFont([PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA]);
+
+		// set default monospaced font
+		$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+		// set margins
+		$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+		$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+		$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+		// set auto page breaks
+		$pdf->SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+
+		// set image scale factor
+		$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+		// set some language-dependent strings (optional)
+		if (@file_exists(dirname(__FILE__) . '/lang/eng.php'))
+		{
+			require_once(dirname(__FILE__) . '/lang/eng.php');
+			$pdf->setLanguageArray($l);
+		}
+
+		// ---------------------------------------------------------
+
+		// set font
+		$pdf->SetFont('times', 'BI', 12);
+
+		// add a page
+		$pdf->AddPage();
+
+		// set some text to print
+		$txt = <<<EOD
+		TCPDF Example 003
+
+		Custom page header and footer are defined by extending the TCPDF class and overriding the Header() and Footer() methods.
+		EOD;
+
+		// print a block of text using Write()
+		$pdf->Write(0, $txt, '', 0, 'C', true, 0, false, false, 0);
+
+		// ---------------------------------------------------------
+
+		//Close and output PDF document
+		$this->response->setContentType('application/pdf');
+		$pdf->Output('example_003.pdf', 'I');
+
+		//============================================================+
+		// END OF FILE
+		//============================================================+
+	}
 }
